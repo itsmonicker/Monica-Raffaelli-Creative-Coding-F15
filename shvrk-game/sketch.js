0d3434ef9e910fad2704e3adabc4ddad0635a815
itsmonicker <m.m.raffaelli@gmail.com>
2015-11-02 21:26:25 -0500

    [game] shark animation
    load p5.play.js for game animation

---
+++ shvrk-game/sketch.js
@@ -0,0 +1,16 @@
+var shark;
+
+function preload() {
+
+shark = loadAnimation("assets/shvrk-game1.jpg", "assets/shvrk-game2.jpg")
+}
+
+function setup() {
+  createCanvas(450,600);
+}
+
+function draw() {
+  background(0);  
+    
+  animation(shark, 500, 150);
+}
